9776626e57c3834a1b85563ec4656ec828f6d7f5
Pavel <savchukpa@icloud.com>
2025-08-28 12:57:21 +0300

    hw_1

---
+++ hw_1.py
@@ -0,0 +1,14 @@
+# part 1
+
+print("Hello World")
+
+# part 2
+
+i_like = "Я люблю:"
+first_string = "Питон"
+two_string = "AI"
+three_string = "Прокрастинировать"
+print(i_like)
+print(f"1. {first_string}")
+print(f"2. {two_string}")
+print(f"3. {three_string}")
